ee1d4f486db299262e6cf7fb042528ba5e248d7b
Your Name <applezafar@gmail.com>
2022-09-25 00:33:45 +1000

    ui, unit tests

--- app/src/androidTest/java/com/example/realmdatabase/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/realmdatabase/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.example.realmdatabase
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,114 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.realmdatabase", appContext.packageName)
+    fun checkAllComponentIsVisible_isSuccess() {
+        Espresso.onView(ViewMatchers.withId(R.id.fabAddContact))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
+
+        val name = "Nikita"
+
+        Espresso.onView(ViewMatchers.withId(R.id.etName))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.typeText(name))
+            .check(ViewAssertions.matches(ViewMatchers.withText(name)))// набирает текст
+
+        val surname = "Panchenko"
+
+        Espresso.onView(ViewMatchers.withId(R.id.etSurname))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.typeText(surname))
+            .check(ViewAssertions.matches(ViewMatchers.withText(surname)))
+
+        val phone = "+799999999"
+
+        Espresso.onView(ViewMatchers.withId(R.id.etNumber))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.typeText(phone))
+            .check(ViewAssertions.matches(ViewMatchers.withText(phone)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.btnSave))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
+
+
+        Espresso.onView(ViewMatchers.withId(R.id.rvContacts))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1,
+                clickChildViewWithId(R.id.tvEdit)))
+
+
+        val newName = "Zafar"
+
+        Espresso.onView(ViewMatchers.withId(R.id.etName))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(setTextInTextView(newName))
+            .check(ViewAssertions.matches(ViewMatchers.withText(newName)))// набирает текст
+
+        val newSurname2 = "Gadzhiev"
+
+        Espresso.onView(ViewMatchers.withId(R.id.etSurname))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(setTextInTextView(newSurname2))
+            .check(ViewAssertions.matches(ViewMatchers.withText(newSurname2)))
+
+        val newPhone = "+78951256198"
+
+        Espresso.onView(ViewMatchers.withId(R.id.etNumber))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(setTextInTextView(newPhone))
+            .check(ViewAssertions.matches(ViewMatchers.withText(newPhone)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.btnSaveChanges))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.fabAddContact))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
+
+        val nameToAdd = "Ruslan"
+
+        Espresso.onView(ViewMatchers.withId(R.id.etName))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.typeText(nameToAdd))
+            .check(ViewAssertions.matches(ViewMatchers.withText(nameToAdd)))// набирает текст
+
+        val surnameToAdd = "Parchiev"
+
+        Espresso.onView(ViewMatchers.withId(R.id.etSurname))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.typeText(surnameToAdd))
+            .check(ViewAssertions.matches(ViewMatchers.withText(surnameToAdd)))
+
+        val phoneToAdd = "+71111111111"
+
+        Espresso.onView(ViewMatchers.withId(R.id.etNumber))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.typeText(phoneToAdd))
+            .check(ViewAssertions.matches(ViewMatchers.withText(phoneToAdd)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.btnSave))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.rvContacts))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(2,
+                clickChildViewWithId(R.id.tvEdit)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.btnDelete))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.rvContacts))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
     }
+
 }
--- app/src/test/java/com/example/realmdatabase/ExampleUnitTest.kt
+++ app/src/test/java/com/example/realmdatabase/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.example.realmdatabase
 
+import com.example.realmdatabase.fakeObjects.FakeContact
+import com.example.realmdatabase.fakeObjects.FakeContactRepository
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,8 +12,72 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    val number = "+88005553535"
+    val contact = FakeContact(
+        name = "TestName",
+        surname = "TestSurname",
+        phone = "88005553535"
+    )
+    val contactRepository = FakeContactRepository()
+
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testAddContact() {
+
+        contactRepository.addContact(contact)
+        val list = contactRepository.getAllContacts()
+        val lastContact = list.last()
+
+        assertEquals(contact, lastContact)
+        assertNotEquals(number, lastContact.phone)
     }
+    @Test
+    fun testDeleteContact() {
+        val contactfirst = FakeContact(
+            name = "TestName",
+            surname = "TestSurname",
+            phone = "88005553535"
+        )
+        val contactSecond = FakeContact(
+            name = "SecondTestName",
+            surname = "SecondTestSurname",
+            phone = "Second88005553535"
+        )
+
+        contactRepository.addContact(contactfirst)
+        contactRepository.addContact(contactSecond)
+        contactRepository.deleteContact(contactSecond)
+        val list = contactRepository.getAllContacts()
+        val lastContact = list.last()
+
+        assertEquals(contactfirst, lastContact)
+
+    }
+
+    @Test
+    fun testSearchContact() {
+        val contactfirst = FakeContact(
+            name = "111111",
+            surname = "11111",
+            phone = "11111111"
+        )
+        val contactSecond = FakeContact(
+            name = "2222222",
+            surname = "2222222",
+            phone = "2222222"
+        )
+        val contactThird = FakeContact(
+            name = "33333",
+            surname = "33333",
+            phone = "33333"
+        )
+
+        contactRepository.addContact(contactfirst)
+        contactRepository.addContact(contactSecond)
+        contactRepository.contactsShown("2")
+        val list = contactRepository.contactsShown("2")
+        assertEquals(1, list.size)
+    }
+
 }
